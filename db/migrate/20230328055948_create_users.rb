0a398d59db20e994282c4524df65a8ddf6a9acb1
Kenta Murata <mrkn@mrkn.jp>
2023-03-28 15:22:15 +0900

    Make User.slack_id not null

--- db/migrate/20230328055948_create_users.rb
+++ db/migrate/20230328055948_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :slack_id
+      t.string :slack_id, null: false
       t.string :name
       t.string :real_name
       t.string :email
