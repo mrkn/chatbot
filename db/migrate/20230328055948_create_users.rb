bc197dc15944f7f93182ed63bc7bd12ff6f410b3
Kenta Murata <mrkn@mrkn.jp>
2023-03-28 15:00:04 +0900

    bin/rails generate model user slack_id:string:uniq name:string real_name:string email:string:uniq tz_offset:integer

---
+++ db/migrate/20230328055948_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :slack_id
+      t.string :name
+      t.string :real_name
+      t.string :email
+      t.integer :tz_offset
+
+      t.timestamps
+    end
+    add_index :users, :slack_id, unique: true
+    add_index :users, :email, unique: true
+  end
+end
