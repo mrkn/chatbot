a7d63b6fa3a53e35b804fdcd25cfd0fe9fd8ed69
Kenta Murata <mrkn@mrkn.jp>
2023-03-30 08:22:37 +0900

    Make Query attributes not null

--- db/migrate/20230329231915_create_queries.rb
+++ db/migrate/20230329231915_create_queries.rb
@@ -2,8 +2,8 @@ class CreateQueries < ActiveRecord::Migration[7.0]
   def change
     create_table :queries do |t|
       t.references :message, null: false, foreign_key: true
-      t.string :text
-      t.jsonb :body
+      t.string :text, null: false
+      t.jsonb :body, null: false
 
       t.timestamps
     end
