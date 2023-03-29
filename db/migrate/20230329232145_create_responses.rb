cea8aee009cb56d7bebacbc4cdffd521a34bc5ad
Kenta Murata <mrkn@mrkn.jp>
2023-03-30 08:23:17 +0900

    Make Response attributes not null

--- db/migrate/20230329232145_create_responses.rb
+++ db/migrate/20230329232145_create_responses.rb
@@ -2,10 +2,10 @@ class CreateResponses < ActiveRecord::Migration[7.0]
   def change
     create_table :responses do |t|
       t.references :query, null: false, foreign_key: true
-      t.string :text
-      t.integer :n_query_tokens
-      t.integer :n_response_tokens
-      t.jsonb :body
+      t.string :text, null: false
+      t.integer :n_query_tokens, null: false
+      t.integer :n_response_tokens, null: false
+      t.jsonb :body, null: false
       t.boolean :good
 
       t.timestamps
